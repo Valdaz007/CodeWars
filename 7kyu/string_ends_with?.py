51c16f9154f0470e33c072ff3dfe611458d11ba0
Victor Volsavai <59840456+Valdaz007@users.noreply.github.com>
2023-02-25 17:53:58 +1000

    Update string_ends_with?.py
    Long Solution with Code Comments

--- 7kyu/string_ends_with?.py
+++ 7kyu/string_ends_with?.py
@@ -5,5 +5,11 @@
 # solution('abc', 'bc') # returns true
 # solution('abc', 'd') # returns false
 
+def long_solution(text, ending):
+  if ending in text[-len(ending):]:     # using the 'len()' we can get the number of characters in the ending variable
+    return True                         # and using text[-len(ending):] - we compare with the last character numbers that correspond to number of characters in ending
+  else:
+    return False
+
 def solution(text, ending):
   return True if ending in text[-len(ending):] else False
