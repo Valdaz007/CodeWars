9355290181a50cae6d82b51c799bcf8a8d64d8d4
Victor Volsavai <59840456+Valdaz007@users.noreply.github.com>
2023-02-28 19:26:49 +1000

    Update persistent_bugger.py
    One liner if & for loop statement

--- 6kyu/persistent_bugger.py
+++ 6kyu/persistent_bugger.py
@@ -9,10 +9,8 @@
 # 4 --> 0 (because 4 is already a one-digit number)
 
 def persistence(n):
-    if n > 9:
-        return 0
+    if n > 9: return 0
     else:
         mul = 1
-        for i in str(n):
-            mul *= int(i)
+        for i in str(n): mul *= int(i)
         return persistence(mul)+1
