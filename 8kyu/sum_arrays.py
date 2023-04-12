86462d2db9753e840480c3931f683565275bfe18
Victor Volsavai <59840456+Valdaz007@users.noreply.github.com>
2023-04-13 09:59:49 +1000

    Update sum_arrays.py
    Included Data Type

--- 8kyu/sum_arrays.py
+++ 8kyu/sum_arrays.py
@@ -1,5 +1,6 @@
 # Kata Title: Sum Arrays
-# Write a function that takes an array of numbers and returns the sum of the numbers. The numbers can be negative or non-integer. If the array does not contain any numbers then you should return 0.
+# Write a function that takes an array of numbers and returns the sum of the numbers. The numbers can be negative or non-integer. 
+# If the array does not contain any numbers then you should return 0.
 
 # Examples
 # Input: [1, 5.2, 4, 0, -1]
@@ -19,5 +20,5 @@
 # We're testing basic loops and math operations. This is for beginners who are just learning loops and math operations.
 # Advanced users may find this extremely easy and can easily write this in one line.
 
-def sum_array(a):
+def sum_array(a: list) -> int:
     return sum(a)
