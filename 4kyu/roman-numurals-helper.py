13c664a1fc4d37b94dd773c8b2ecc59f081a5105
Valdaz007 <59840456+Valdaz007@users.noreply.github.com>
2023-12-10 08:29:08 +1000

    Updated 4ky-roman-numerals-helper

--- 4kyu/roman-numurals-helper.py
+++ 4kyu/roman-numurals-helper.py
@@ -1,5 +1,4 @@
 class RomanNumerals:
-    
     romnum = {"M":1000, "CM":900, "D":500, "CD":400, "C":100, "XC":90, "L":50, "XL":40, "X":10, "IX":9, "V":5, "IV":4, "I": 1}
     
     @staticmethod
@@ -8,18 +7,20 @@ class RomanNumerals:
         while val != 0:
             for i in RomanNumerals.romnum: 
                 if RomanNumerals.romnum[i] <= val: 
-                    val=val-RomanNumerals.romnum[i]
+                    val-=RomanNumerals.romnum[i]
                     romlist.append(i)
                     break
         return "".join(romlist)
     
     @staticmethod
-    def from_roman(roman_num : str):
+    def from_roman(roman_num : str) -> int:
         rom_sum = RomanNumerals.romnum[roman_num[-1]]
         
-        for i,j in zip(roman_num[1::][::-1],roman_num[:-1:][::-1]):
+        for i,j in zip(roman_num[1:],roman_num[:-1]):
             if RomanNumerals.romnum[i] <= RomanNumerals.romnum[j]:
                 rom_sum+=RomanNumerals.romnum[j]
             else:
                 rom_sum-=RomanNumerals.romnum[j]
         return rom_sum
+    
+print(RomanNumerals.from_roman("MCMI"))
