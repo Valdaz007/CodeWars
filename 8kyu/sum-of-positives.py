4b325a583e2f407239726924f17e88eec8c2b2ee
Valdaz007 <59840456+Valdaz007@users.noreply.github.com>
2023-12-11 07:15:11 +1000

    Added 8kyu entry: sum-of-positives.py

---
+++ 8kyu/sum-of-positives.py
@@ -0,0 +1,12 @@
+## Description
+# You get an array of numbers, return the sum of all of the positives ones.
+
+# Example [1,-4,7,12] => 1 + 7 + 12 = 20
+
+# Note: if there is nothing to sum, the sum is default to 0.
+
+def positive_sum(arr):
+    return sum(i for i in arr if i > 0)
+
+#Test
+print(positive_sum([1,-4,7,12]))
