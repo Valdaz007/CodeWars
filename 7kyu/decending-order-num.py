48878720926f759c60770b02ba0b15adf183395d
Valdaz007 <59840456+Valdaz007@users.noreply.github.com>
2023-12-10 23:24:56 +1000

    Added 7kyu entry: descending-order-num.py

---
+++ 7kyu/decending-order-num.py
@@ -0,0 +1,10 @@
+## Discription
+# Your task is to make a function that can take any non-negative integer as an argument and return it with its digits in descending order. 
+# Essentially, rearrange the digits to create the highest possible number.
+
+def descending_order(num: int) -> int:
+    return int("".join([i for i in sorted(str(num))[::-1]]))
+
+
+#Test
+print(descending_order(357832))
