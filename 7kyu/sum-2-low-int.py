584f5aedcc91f13093eba9e76265c7e284dd3313
valdaz007 <volsavaivictor@gmail.com>
2023-12-11 15:56:29 +1000

    Added kata description: sum-2-low-int.py

--- 7kyu/sum-2-low-int.py
+++ 7kyu/sum-2-low-int.py
@@ -1,4 +1,10 @@
+## Description
+# Create a function that returns the sum of the two lowest positive numbers given an array of minimum 4 positive integers. 
+# No floats or non-positive integers will be passed.
+# For example, when an array is passed like [19, 5, 42, 2, 77], the output should be 7.
+
 def sum_two_smallest_numbers(numbers):
     return sum(sorted(numbers)[0:2])
 
+# Test
 print(sum_two_smallest_numbers([8,2,9,1]))
